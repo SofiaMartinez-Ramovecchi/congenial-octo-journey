fb2cd706669aac70331d794b6a46a3cd8acb978f
SofiaMartinez-Ramovecchi <zunildaramivecchi@gmail.com>
2023-08-22 11:08:37 -0300

    segunda iteracion import

--- screem.py
+++ screem.py
@@ -1,7 +1,10 @@
 import os
+import threading
 
 # Captura de pantalla usando el comando import
+
 window_id = os.popen("xwininfo | grep 'Window id:' | cut -d' ' -f4").read().strip()
+
 screenshot_filename = input("Introduce el nombre del archivo de captura de pantalla: ")
 
 # Ejecutar el comando de captura de pantalla y guardarla con el nombre proporcionado
